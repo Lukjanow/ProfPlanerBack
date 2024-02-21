6359543dbcc45283e9b1136916e31c3adf586243
Julius Müller <julius.matthias.mueller@gmail.com>
2024-02-21 10:55:58 +0100

    Fix Warning - Pydantic Config

--- models/common.py
+++ models/common.py
@@ -1,12 +1,19 @@
-from pydantic import BaseModel
+from typing import ClassVar
+from pydantic import BaseModel, ConfigDict
 
 class HTTPError(BaseModel):
     detail: str
 
-    class Config:
-        schema_extra = {
-            "example": {"detail": "HTTPException raised."},
-        }
+    # class Config(ConfigDict):
+    #     json_schema_extra = {
+    #         "example": {"detail": "HTTPException raised."},
+    #     }
+
+    Config: ClassVar = ConfigDict(json_schema_extra={
+        "example": {"detail": "HTTPException raised."}
+    })
+
+
 
 
 class Message(BaseModel):
