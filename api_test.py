719595dc0bee8d44b6675cda3ae072bd85f31881
Yannick Stock <inf3896@hs-worms.de>
2024-02-26 11:47:21 +0100

    fix room test (working with Object ID)

--- api_test.py
+++ api_test.py
@@ -1,9 +1,11 @@
+import re
 import pytest
 from fastapi.testclient import TestClient
 from API import app  # import your FastAPI app
 
 # Setup TestClient for FastAPI
 client = TestClient(app)
+room_id = ""
 
 # Test for the index route
 def test_index():
@@ -16,33 +18,41 @@ def test_index():
 
 def test_create_room():
     response = client.post("/room/add", json=
-                           {"id": 98, "name": "TestRoom", "capacity": 100, "equipment": 1}
+                           {"name": "TestRoom", "capacity": 100, "equipment": 1}
                         )
     assert response.status_code == 200
 
 
 def test_update_room():
+    global room_id
     response = client.post("/room/add", json=
-                           {"id": 99, "name": "TestRoom", "capacity": 100, "equipment": 1}
+                           {"_id": "", "name": "TestRoom", "capacity": 100, "equipment": 1}
                         )
+    
+    object_id_pattern = re.compile(r"ObjectId\('(\w+)'\)")
+    match = object_id_pattern.search(response.json()["message"])
+    room_id = match.group(1)
+    
     print(response.text)
     assert response.status_code == 200
 
-    response = client.put("/room/99?name=N43")
+    response = client.put(f"/room/{room_id}",json={"name":"N43"})
     print(response.text)
     assert response.status_code == 200, "Name konnte nicht geupdatet werden"
 
-    response = client.put("/room/99?capacity=101")
+    response = client.put(f"/room/{room_id}",json={"capacity":101})
     print(response.text)
     assert response.status_code == 200, "Kapazität konnte nicht geupdatet werden"
 
-    response = client.put("/room/99?equipment=2")
+    response = client.put(f"/room/{room_id}",json={"equipment":2})
     print(response.text)
     assert response.status_code == 200, "Equipment konnte nicht geupdatet werden"
 
 
 def test_delete_room():
-    response = client.delete("/room/99")
+    global room_id
+
+    response = client.delete(f"/room/{room_id}")
     assert response.status_code == 200, "Raum konnte nicht gelöscht werden"
 
     
