f0c84878811b0338b08f8ac9149f90788549ae37
Yannick Stock <inf3896@hs-worms.de>
2024-03-01 13:20:32 +0100

    update Module when room delete

--- routes/room.py
+++ routes/room.py
@@ -11,6 +11,7 @@ router = APIRouter()
 from Database.Database import db
 
 rooms = db["rooms"]
+modules = db["modules"]
 
 @router.get("/room",summary="read all Room",
         description="Get all Rooms from Database. Returns an Array of Json's.",
@@ -119,5 +120,19 @@ async def Delete_Room(room_id):
     except:
         raise HTTPException(400, detail=f'{room_id} is not a valid ObjectId, it must be a 12-byte input or a 24-character hex string',)
     
+
+    reList = modules.find({"room": {"$elemMatch": {"$eq": room_id}}})
+    for module in reList:
+        newRoomList = []
+        for room in module["room"]:
+            if room == room_id:
+                continue
+            newRoomList.append(room)
+        modules.update_one({"_id":module["_id"]}, {"$set":{"room":newRoomList}})
+        
+    reList = modules.find({"room":room_id})
+    for module in reList:
+        modules.update_one({"_id":module["_id"]}, {"$set":{"room":None}})
+
     rooms.delete_one({"_id": id})
-    return {"message": f"Successfully deleted Dozent {room_id}"}
+    return {"message": f"Successfully deleted Room {room_id}"}
