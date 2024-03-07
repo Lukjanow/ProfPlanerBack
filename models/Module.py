e4bf3f9e570f19701a1c4ea79a0f6a23fb6d9bd1
Timo_B <timo.buehler01@gmx.de>
2024-03-07 11:16:30 +0100

    change room route

--- models/Module.py
+++ models/Module.py
@@ -3,7 +3,6 @@ from pydantic import BaseModel
 from models.Dozent import Dozent
 from models.Room import Room
 from models.StudySemester import StudySemester
-from models.enums.Equipment import Equipment
 from models.enums.Type import Type
 from models.enums.Frequency import Frequency
 
@@ -17,7 +16,6 @@ class Module(BaseModel, use_enum_values=True):
     study_semester: list[StudySemester]
     duration: int
     approximate_attendance: int
-    need: Equipment | None
     type: list[Type]
     frequency: Frequency
     selected: bool
