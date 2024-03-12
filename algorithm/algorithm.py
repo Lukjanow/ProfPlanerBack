58feb6ed39f757615720c0b96a3369d04f09d1d8
Pierre Nitschke <inf3903@hs-worms.de>
2024-03-12 14:02:12 +0100

    input modules

--- algorithm/algorithm.py
+++ algorithm/algorithm.py
@@ -1,18 +1,37 @@
-from conflicts import checkTimetableForConflicts
-from routes.modules import convertDataWithReferences
-from bson import ObjectId
-from Database.Database import db
+import sys
+import os
+print("HEX")
+# Get the current directory of the script
+current_dir = os.path.dirname(os.path.realpath(__file__))
+print("HEX")
+# Get the parent directory
+parent_dir = os.path.abspath(os.path.join(current_dir, os.pardir))
+print("HEX")
+# Add the parent directory to sys.path
+sys.path.append(parent_dir)
+print("HEX")
+# Now you can import modules from the 'routes' folder
 
+# Now you can use functions or classes from routes.modules
+
+
+from conflicts import checkTimetableForConflicts
+from bson import ObjectId
+from routes.modules import convertDataWithReferences
+from Database.Database import db
+print("HEX")
 modules = db["modules"]
 calendarentry = db["calendarEntry"]
 calendars = db["calendar"]
-
+print("HAX")
 #TODO: GET ALL NOT PLANNED MODULES
-re = modules.find()   
+re = modules.find()
+print("HOX")
 module_list = convertDataWithReferences(re)
-
+print("HOX")
 result = calendars.find_one(ObjectId("65d61765c15324dcfc497c4f"))
 calendarEntrys = []
+print("HEX")
 for entry in result["entries"]:
     entryData = calendarentry.find_one(ObjectId(entry))
     moduledata = modules.find_one(ObjectId(entryData["module"]))
